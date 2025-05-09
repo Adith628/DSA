5cd32997f354077b54ab4c2f6a0148d2ce993c86
Adith628 <adithsreedhar628@gmail.com>
2025-05-09 22:06:27 +0530

    gcd

--- BasicMath/gcd.cpp
+++ BasicMath/gcd.cpp
@@ -2,10 +2,11 @@
 using namespace std;
 
 int euclid(int n, int m);
+int gcdt(int n, int m);
 
 int main()
 {
-    int n = 4, m = 14;
+    int n = 10, m = 20;
     int gcd = 1;
     for (int i = 1; i <= min(n, m); i++)
     {
@@ -15,6 +16,8 @@ int main()
     cout << "gcd = " << gcd << endl;
     int x = euclid(n, m);
     cout << "gcd2 = " << x << endl;
+    int y = gcdt(n, m);
+    cout << "gcd3 = " << y << endl;
     return 0;
 }
 
@@ -23,4 +26,15 @@ int euclid(int n, int m)
     if (m == 0)
         return n;
     return euclid(m, n % m);
+}
+
+int gcdt(int a, int b)
+{
+    while (b != 0)
+    {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
 }
